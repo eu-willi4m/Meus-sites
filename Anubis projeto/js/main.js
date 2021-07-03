52401f64b00ae0f5d4bfa98c6c267384d4ce83fe
eu-willi4m <86448052+eu-willi4m@users.noreply.github.com>
2021-07-03 01:57:27 -0300

    meu "primeiro" site ; )

---
+++ Anubis projeto/js/main.js
@@ -0,0 +1,10 @@
+$('.slider-pricipal').slick({
+    dots: true, 
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptverHeigth: true,
+    autoplay: true,
+    autoplaySpeed: 2700
+
+});
